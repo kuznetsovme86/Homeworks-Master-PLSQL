8e0b27bb3437c3b4b8c3e7b0aa01575179425213
kuznetsovme86 <kuznetsov_me86@vk.com>
2025-05-10 13:41:45 +0300

    Hw4 (#4)
    * Homework 1 and 2
    
    * Домашка 3
    
    * test
    
    * update hw3
    
    * update hw3 utf8
    
    * HomeWork4

--- hw3/hw.sql
+++ hw3/hw.sql
@@ -5,56 +5,68 @@
 
 --Создание платежа.
 declare
-  v_payment_action_message varchar2(300 char):= 'Платеж создан. '; 
-  c_status_payment_success_creation constant number(1):=0;
+  v_payment_action_message varchar2(200 char):= 'Платеж создан. '; 
+  c_status_payment_success_creation constant number(10):=0;
+  v_current_dtime date:= sysdate;
 begin
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_creation||'.');
+  dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24'));
 end;
 /
 
 --Сброс платежа в "ошибочный статус".
 declare
-  v_payment_action_message varchar2(300 char):= 'Сброс платежа в "ошибочный статус" с указанием причины. '; 
-  c_status_payment_reset_error constant number(1):=2;
-  v_reason_message varchar2(300 char):= 'Причина: недостаточно средств.';
+  v_payment_action_message varchar2(200 char):= 'Сброс платежа в "ошибочный статус" с указанием причины. '; 
+  c_status_payment_reset_error constant number(10):=2;
+  v_reason_message varchar2(200 char):= 'Причина: недостаточно средств.';
+  v_current_dtime date:= sysdate;
 begin
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_reset_error|| '. '||v_reason_message);
+  dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24:mi:ss'));
 end;
 /
 
 --Отмена платежа.
 declare
-  v_payment_action_message varchar2(300 char):= 'Отмена платежа с указанием причины. '; 
-  c_status_payment_cancel constant number(1):=3;
-  v_reason_message varchar2(300 char):= 'Причина: ошибка пользователя.';
+  v_payment_action_message varchar2(200 char):= 'Отмена платежа с указанием причины. '; 
+  c_status_payment_cancel constant number(10):=3;
+  v_reason_message varchar2(200 char):= 'Причина: ошибка пользователя.';
+  v_current_dtime date:= sysdate;
 begin
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_cancel|| '. '||v_reason_message);
+  dbms_output.put_line(to_char(v_current_dtime,'day-mon-yy'));
 end;
 /
 
 --Успешное завершение платежа.
 declare
-  v_payment_action_message varchar2(300 char):= 'Успешное завершение платежа. '; 
-  c_status_payment_success_end constant number(1):=1;
+  v_payment_action_message varchar2(200 char):= 'Успешное завершение платежа. '; 
+  c_status_payment_success_end constant number(10):=1;
+  v_current_dtime date:= sysdate;
 begin
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_end||'.');
+  dbms_output.put_line(to_char(v_current_dtime,'WW-Q-yy:hh24:mi->ss'));
 end;
 /
 
 --Данные платежа добавлены или обновлены.
 declare
-  v_data_payment_action_message varchar2(300 char):= 'Данные платежа добавлены или обновлены '; 
-  c_payment_param_list_id_value constant varchar2(300 char):='по списку id_поля/значение.';
+  v_data_payment_action_message varchar2(200 char):= 'Данные платежа добавлены или обновлены '; 
+  c_payment_param_list_id_value constant varchar2(200 char):='по списку id_поля/значение.';
+  v_current_dts timestamp:= systimestamp;
 begin
   dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id_value);
+  dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff6'));
 end;
 /
 
 --Детали платежа удалены.
 declare
-  v_data_payment_action_message varchar2(300 char):= 'Детали платежа удалены '; 
-  c_payment_param_list_id constant varchar2(300 char):='по списку id_полей.';
+  v_data_payment_action_message varchar2(200 char):= 'Детали платежа удалены '; 
+  c_payment_param_list_id constant varchar2(200 char):='по списку id_полей.';
+  v_current_dts timestamp:= systimestamp;
 begin
   dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id);
+  dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff9'));
 end;
 /
