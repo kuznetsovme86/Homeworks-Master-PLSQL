07a1b3065c058bdfc972d77566b3de44ee6353d8
kuznetsovme86 <kuznetsov_me86@vk.com>
2025-05-10 19:11:41 +0300

    Homework 5

--- hw3/hw.sql
+++ hw3/hw.sql
@@ -8,7 +8,9 @@ declare
   v_payment_action_message varchar2(200 char):= 'Платеж создан. '; 
   c_status_payment_success_creation constant number(10):=0;
   v_current_dtime date:= sysdate;
+  v_payment_id  number(38) := 1;
 begin
+  dbms_output.put_line('v_payment_id='||v_payment_id);
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_creation||'.');
   dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24'));
 end;
@@ -20,9 +22,17 @@ declare
   c_status_payment_reset_error constant number(10):=2;
   v_reason_message varchar2(200 char):= 'Причина: недостаточно средств.';
   v_current_dtime date:= sysdate;
+  v_payment_id  number(38) := 105;
 begin
-  dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_reset_error|| '. '||v_reason_message);
-  dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24:mi:ss'));
+  if v_payment_id is not null then
+    if v_reason_message is not null then
+        dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_reset_error|| '. '||v_reason_message);
+        dbms_output.put_line(to_char(v_current_dtime,'dd.mm.yyyy hh24:mi:ss'));
+        dbms_output.put_line('v_payment_id='||v_payment_id);
+    else dbms_output.put_line('Причина не может быть пустой.');
+    end if;
+  else dbms_output.put_line('ID объекта не может быть пустым');
+  end if;
 end;
 /
 
@@ -32,9 +42,15 @@ declare
   c_status_payment_cancel constant number(10):=3;
   v_reason_message varchar2(200 char):= 'Причина: ошибка пользователя.';
   v_current_dtime date:= sysdate;
+  v_payment_id  number(38) := 777;
 begin
+  if v_payment_id is null then dbms_output.put_line('ID объекта не может быть пустым');
+  elsif v_reason_message is null then dbms_output.put_line('Причина не может быть пустой.');
+  else 
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_cancel|| '. '||v_reason_message);
   dbms_output.put_line(to_char(v_current_dtime,'day-mon-yy'));
+  dbms_output.put_line('v_payment_id='||v_payment_id);
+  end if;
 end;
 /
 
@@ -43,9 +59,14 @@ declare
   v_payment_action_message varchar2(200 char):= 'Успешное завершение платежа. '; 
   c_status_payment_success_end constant number(10):=1;
   v_current_dtime date:= sysdate;
+  v_payment_id  number(38) := 55;
 begin
+  if v_payment_id is not null then
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_end||'.');
   dbms_output.put_line(to_char(v_current_dtime,'WW-Q-yy:hh24:mi->ss'));
+  dbms_output.put_line('v_payment_id='||v_payment_id);
+  else dbms_output.put_line('ID объекта не может быть пустым');
+  end if;
 end;
 /
 
@@ -54,9 +75,15 @@ declare
   v_data_payment_action_message varchar2(200 char):= 'Данные платежа добавлены или обновлены '; 
   c_payment_param_list_id_value constant varchar2(200 char):='по списку id_поля/значение.';
   v_current_dts timestamp:= systimestamp;
+  v_payment_id  number(38) := 55;
 begin
-  dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id_value);
-  dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff6'));
+  case v_payment_id
+    when null then dbms_output.put_line('ID объекта не может быть пустым');
+  else 
+    dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id_value);
+    dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff6'));
+    dbms_output.put_line('v_payment_id='||v_payment_id);
+  end case;
 end;
 /
 
@@ -65,8 +92,14 @@ declare
   v_data_payment_action_message varchar2(200 char):= 'Детали платежа удалены '; 
   c_payment_param_list_id constant varchar2(200 char):='по списку id_полей.';
   v_current_dts timestamp:= systimestamp;
+  v_payment_id  number(38);
 begin
-  dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id);
-  dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff9'));
+  case 
+  when v_payment_id is not null then 
+    dbms_output.put_line(v_data_payment_action_message||c_payment_param_list_id);
+    dbms_output.put_line(to_char(v_current_dts,'dd.mm.yyyy hh24:mi:ss.ff9'));
+    dbms_output.put_line('v_payment_id='||v_payment_id);
+  else dbms_output.put_line('ID объекта не может быть пустым');
+  end case;
 end;
 /
