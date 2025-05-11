9f795a790541c2e19748c1ef5aa9f793a05f2679
kuznetsovme86 <kuznetsov_me86@vk.com>
2025-05-11 18:26:37 +0300

    Homework 6

--- hw3/hw.sql
+++ hw3/hw.sql
@@ -6,9 +6,9 @@
 --Создание платежа.
 declare
   v_payment_action_message varchar2(200 char):= 'Платеж создан. '; 
-  c_status_payment_success_creation constant number(10):=0;
+  c_status_payment_success_creation payment.status%type:=0;
   v_current_dtime date:= sysdate;
-  v_payment_id  number(38) := 1;
+  v_payment_id  payment.payment_id%type := 1;
 begin
   dbms_output.put_line('v_payment_id='||v_payment_id);
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_creation||'.');
@@ -19,10 +19,10 @@ end;
 --Сброс платежа в "ошибочный статус".
 declare
   v_payment_action_message varchar2(200 char):= 'Сброс платежа в "ошибочный статус" с указанием причины. '; 
-  c_status_payment_reset_error constant number(10):=2;
-  v_reason_message varchar2(200 char):= 'Причина: недостаточно средств.';
+  c_status_payment_reset_error constant payment.status%type:=2;
+  v_reason_message payment.status_change_reason%type:= 'Причина: недостаточно средств.';
   v_current_dtime date:= sysdate;
-  v_payment_id  number(38) := 105;
+  v_payment_id  payment.payment_id%type := 105;
 begin
   if v_payment_id is not null then
     if v_reason_message is not null then
@@ -39,10 +39,10 @@ end;
 --Отмена платежа.
 declare
   v_payment_action_message varchar2(200 char):= 'Отмена платежа с указанием причины. '; 
-  c_status_payment_cancel constant number(10):=3;
-  v_reason_message varchar2(200 char):= 'Причина: ошибка пользователя.';
+  c_status_payment_cancel constant payment.status%type:=3;
+  v_reason_message payment.status_change_reason%type:= 'Причина: ошибка пользователя.';
   v_current_dtime date:= sysdate;
-  v_payment_id  number(38) := 777;
+  v_payment_id  payment.payment_id%type := 777;
 begin
   if v_payment_id is null then dbms_output.put_line('ID объекта не может быть пустым');
   elsif v_reason_message is null then dbms_output.put_line('Причина не может быть пустой.');
@@ -57,9 +57,9 @@ end;
 --Успешное завершение платежа.
 declare
   v_payment_action_message varchar2(200 char):= 'Успешное завершение платежа. '; 
-  c_status_payment_success_end constant number(10):=1;
+  c_status_payment_success_end constant payment.status%type:=1;
   v_current_dtime date:= sysdate;
-  v_payment_id  number(38) := 55;
+  v_payment_id  payment.payment_id%type := 55;
 begin
   if v_payment_id is not null then
   dbms_output.put_line(v_payment_action_message||'Статус: '||c_status_payment_success_end||'.');
@@ -75,7 +75,7 @@ declare
   v_data_payment_action_message varchar2(200 char):= 'Данные платежа добавлены или обновлены '; 
   c_payment_param_list_id_value constant varchar2(200 char):='по списку id_поля/значение.';
   v_current_dts timestamp:= systimestamp;
-  v_payment_id  number(38) := 55;
+  v_payment_id  payment.payment_id%type := 55;
 begin
   case v_payment_id
     when null then dbms_output.put_line('ID объекта не может быть пустым');
@@ -92,7 +92,7 @@ declare
   v_data_payment_action_message varchar2(200 char):= 'Детали платежа удалены '; 
   c_payment_param_list_id constant varchar2(200 char):='по списку id_полей.';
   v_current_dts timestamp:= systimestamp;
-  v_payment_id  number(38);
+  v_payment_id  payment.payment_id%type;
 begin
   case 
   when v_payment_id is not null then 
